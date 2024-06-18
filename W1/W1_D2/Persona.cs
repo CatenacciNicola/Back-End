6c99f44276d189f6eb437ecf4003112576fc393b
Catenacci Nicola <catenaccinicola@outlook.it>
2024-06-18 16:59:19 +0200

    .

---
+++ W1/W1_D2/Persona.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1_D2
+{
+    internal class Persona
+    {
+        string nome;
+        string cognome;
+        int eta;
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public string Cognome
+        {
+            get { return cognome; }
+            set { cognome = value; }
+        }
+        public int Eta
+        {
+            get { return eta; }
+            set { eta = value; }
+        }
+        public void GetNome()
+        {
+            Console.WriteLine(nome);
+        }
+        public void GetCognome()
+        {
+            Console.WriteLine(cognome);
+        }
+        public void GetEta()
+        {
+            Console.WriteLine(eta);
+        }
+        public void GetDettagli()
+        {
+            Console.Write("Mi chiamo ");
+            Console.Write(nome);
+            Console.Write(cognome);
+            Console.Write(" e ho ");
+            Console.Write(eta);
+            Console.Write("anni");
+
+        }
+    }
+}
---
+++ W1/W1_D2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace W1_D2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.Nome = "Nicola";
+            persona.Cognome = "Catenacci";
+            persona.Eta = 29;
+            persona.GetDettagli();
+        }
+    }
+}
